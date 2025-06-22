d7127bcc1c82fac3b6f69d6e8b92846bbd03d258
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2025-06-22 12:14:21 +0530

    MoodType new Added

--- src/data/moodSongs.ts
+++ src/data/moodSongs.ts
@@ -1,47 +1,59 @@
-export type MoodType = "Happy" | "Sad" | "Angry" | "Chill" | "Energetic";
+export type MoodType =
+  | "Happy"
+  | "Sad"
+  | "Angry"
+  | "Chill"
+  | "Energetic"
+  | "Special";
 
 export const moodSongs: Record<MoodType, string[]> = {
   Happy: [
-    "https://www.youtube.com/embed/Z8IRRphKFZA", 
-    "https://www.youtube.com/embed/XVEVM-K98To", 
-    "https://www.youtube.com/embed/Y_NDf2dCO4E", 
+    "https://www.youtube.com/embed/Z8IRRphKFZA",
+    "https://www.youtube.com/embed/XVEVM-K98To",
+    "https://www.youtube.com/embed/Y_NDf2dCO4E",
     "https://www.youtube.com/embed/cWMxCE2HTag",
-    "https://www.youtube.com/embed/nCD2hj6zJEc", 
-    "https://www.youtube.com/embed/3wGLsjgTx4c", 
+    "https://www.youtube.com/embed/nCD2hj6zJEc",
+    "https://www.youtube.com/embed/3wGLsjgTx4c",
   ],
   Sad: [
-    "https://www.youtube.com/embed/Z8IRRphKFZA", 
-    "https://www.youtube.com/embed/XVEVM-K98To", 
-    "https://www.youtube.com/embed/Y_NDf2dCO4E", 
+    "https://www.youtube.com/embed/Z8IRRphKFZA",
+    "https://www.youtube.com/embed/XVEVM-K98To",
+    "https://www.youtube.com/embed/Y_NDf2dCO4E",
     "https://www.youtube.com/embed/cWMxCE2HTag",
-    "https://www.youtube.com/embed/nCD2hj6zJEc", 
-    "https://www.youtube.com/embed/3wGLsjgTx4c", 
+    "https://www.youtube.com/embed/nCD2hj6zJEc",
+    "https://www.youtube.com/embed/3wGLsjgTx4c",
   ],
   Angry: [
-    "https://www.youtube.com/embed/Z8IRRphKFZA", 
-    "https://www.youtube.com/embed/XVEVM-K98To", 
-    "https://www.youtube.com/embed/Y_NDf2dCO4E", 
+    "https://www.youtube.com/embed/Z8IRRphKFZA",
+    "https://www.youtube.com/embed/XVEVM-K98To",
+    "https://www.youtube.com/embed/Y_NDf2dCO4E",
     "https://www.youtube.com/embed/cWMxCE2HTag",
-    "https://www.youtube.com/embed/nCD2hj6zJEc", 
-    "https://www.youtube.com/embed/3wGLsjgTx4c", 
+    "https://www.youtube.com/embed/nCD2hj6zJEc",
+    "https://www.youtube.com/embed/3wGLsjgTx4c",
   ],
   Chill: [
-    "https://www.youtube.com/embed/Z8IRRphKFZA", 
-    "https://www.youtube.com/embed/XVEVM-K98To", 
-    "https://www.youtube.com/embed/Y_NDf2dCO4E", 
+    "https://www.youtube.com/embed/Z8IRRphKFZA",
+    "https://www.youtube.com/embed/XVEVM-K98To",
+    "https://www.youtube.com/embed/Y_NDf2dCO4E",
     "https://www.youtube.com/embed/cWMxCE2HTag",
-    "https://www.youtube.com/embed/nCD2hj6zJEc", 
-    "https://www.youtube.com/embed/3wGLsjgTx4c", 
+    "https://www.youtube.com/embed/nCD2hj6zJEc",
+    "https://www.youtube.com/embed/3wGLsjgTx4c",
   ],
   Energetic: [
-    "https://www.youtube.com/embed/Z8IRRphKFZA", 
-    "https://www.youtube.com/embed/XVEVM-K98To", 
-    "https://www.youtube.com/embed/Y_NDf2dCO4E", 
+    "https://www.youtube.com/embed/Z8IRRphKFZA",
+    "https://www.youtube.com/embed/XVEVM-K98To",
+    "https://www.youtube.com/embed/Y_NDf2dCO4E",
     "https://www.youtube.com/embed/cWMxCE2HTag",
-    "https://www.youtube.com/embed/nCD2hj6zJEc", 
-    "https://www.youtube.com/embed/3wGLsjgTx4c", 
+    "https://www.youtube.com/embed/nCD2hj6zJEc",
+    "https://www.youtube.com/embed/3wGLsjgTx4c",
+  ],
+  Special: [
+    "https://www.youtube.com/embed/91kbNw9DZBg",
+    "https://www.youtube.com/embed/pElk1ShPrcE",
+    "https://www.youtube.com/embed/1cDoRqPnCXU",
+    "https://www.youtube.com/embed/A04WawrDblo",
+    "https://www.youtube.com/embed/sIooFGRBZJY",
+    "https://www.youtube.com/embed/W9mrvDWUR9g",
+
   ],
 };
-
-
-
